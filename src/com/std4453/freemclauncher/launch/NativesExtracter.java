d8d7f78fb4746af122636dd82ced6de5a623fd1c
std4453 <std4453@foxmail.com>
2015-07-08 22:47:42 +0800

    Coding in progress - Now with natives extracion!

---
+++ src/com/std4453/freemclauncher/launch/NativesExtracter.java
@@ -0,0 +1,56 @@
+package com.std4453.freemclauncher.launch;
+
+import java.io.File;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Expand;
+
+import com.std4453.freemclauncher.files.DirectoryHelper;
+import com.std4453.freemclauncher.libraries.Library;
+import com.std4453.freemclauncher.libraries.LibraryIndexer;
+import com.std4453.freemclauncher.profiles.Profile;
+
+public class NativesExtracter {
+	public NativesExtracter() {
+	}
+
+	public String extractNativesForProfile(Profile profile) {
+		LibraryIndexer indexer = new LibraryIndexer();
+		indexer.getLibrariesOfVersion(profile.getName());
+
+		List<Library> natives = new Vector<Library>();
+
+		for (Library library : indexer.getLibrariesOfVersion(profile.getName())) {
+			if (library.getNatives() != null && !library.getNatives().isEmpty())
+				natives.add(library);
+		}
+
+		String extractDir = "natives-" + profile.getName() + "-"
+				+ System.currentTimeMillis();
+		File extractDst = new File(DirectoryHelper.temp, extractDir);
+		extractDst.mkdirs();
+
+		for (Library library : natives) {
+			String libraryName = library.getNatives();
+
+			Project proj = new Project();
+			Expand expand = new Expand();
+			expand.setProject(proj);
+			expand.setTaskName("unzip");
+			expand.setTaskType("unzip");
+
+			File nativesFile = new File(DirectoryHelper.libraries, libraryName);
+			expand.setSrc(nativesFile);
+			expand.setEncoding("UTF-8");
+
+			expand.setDest(extractDst);
+			expand.execute();
+		}
+
+		extractDst.deleteOnExit();
+
+		return extractDst.getAbsolutePath();
+	}
+}
